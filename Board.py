700016eea554906dd2bba87e6b9323b876657294
Joe <0xbadacce55@gmail.com>
2020-05-28 21:45:55 -0500

    Finished Board (maybe)

--- Board.py
+++ Board.py
@@ -1,20 +1,39 @@
+import Constants
+
 class Board:
     def __init__(self):
-        self.rows[RED] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
-        self.furthest[RED] = 1
-        self.rows[YELLOW] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
-        self.furthest[YELLOW] = 1
-        self.rows[BLUE] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
-        self.furthest[BLUE] = 1
-        self.rows[GREEN] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
-        self.furthest[GREEN] = 1
+        self.rows = []
+        self.furthest = []
+        self.rows[Constants.RED] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
+        self.furthest[Constants.RED] = 1
+        self.rows[Constants.YELLOW] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
+        self.furthest[Constants.YELLOW] = 1
+        self.rows[Constants.BLUE] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
+        self.furthest[Constants.BLUE] = 1
+        self.rows[Constants.GREEN] = {2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False, 9: False, 10: False, 11:False, 12: False}
+        self.furthest[Constants.GREEN] = 1
         self.fehlwurfe = 0
 
-    def move(color, number):
-        pass
+    def is_legal_move(self, color, number) -> bool:
+        if number < 12:
+            if self.furthest[color] < number:
+                return True
+        else:
+            if self.count_strikes(color) > 4:
+                return True
+        return False
 
-    def is_legal_move(color, number):
-        pass
+    def move(self, color, number) -> bool:
+        if self.is_legal_move(color, number):
+            self.rows[color][number] = True
+            self.furthest[color] = number
+            return True
+        else:
+            return False
 
-    def count_strikes(color):
-        pass
+    def count_strikes(self, color) -> int:
+        strikes = 0
+        for number in self.rows[color]:
+            if number is True:
+                strikes += 1
+        return strikes
